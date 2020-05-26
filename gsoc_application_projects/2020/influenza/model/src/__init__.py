135c494fe02bd78213a75a60fe655e858b5ece4f
tejsukhatme <f20180020@hyderabad.bits-pilani.ac.in>
2020-05-26 20:47:21 +0530

    Add process.py to apply features engineering to data.

--- gsoc_application_projects/2020/influenza/model/src/__init__.py
+++ gsoc_application_projects/2020/influenza/model/src/__init__.py
@@ -1,14 +1,12 @@
 from clean import *
 from combine import *
+from model import *
 from prepare import *
 from process import *
-
-# from model import *
-
 
 if __name__ == '__main__':
     combine_data()
     clean_data()
     prepare_data()
     process_data()
-    # apply_regression()
+    apply_regression()
